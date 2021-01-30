ff32c9b92bea30e504f398f65c9919716926f8c9
Morgan Tighe <morgan.tighe@gmail.com>
2021-01-29 23:08:31 -0600

    Counting Votes!

--- PyPoll/PyPoll.py
+++ PyPoll/PyPoll.py
@@ -4,11 +4,29 @@ import os
 
 #files in and out using full path because my computer isn't running os.path.join
 #AskBCS helped me get these lines to run
-import_budget_data ="C:\\Users\\morga\\Repositories\\PythonProject\\PyPoll\\Resources\\election_data.csv"
+import_election_data ="C:\\Users\\morga\\Repositories\\PythonProject\\PyPoll\\Resources\\election_data.csv"
 output_budget_analysis ="C:\\Users\\morga\\Repositories\\PythonProject\\PyPoll\\output\\electionanalysis.txt"
 
 #Counting Votes and defining variables
 vote_count = 0
+
+
+
+#CSV Reading...
+with open(import_election_data) as election_data:
+    read_csv = csv.reader(election_data)
+
+    #Header Row
+    header = next(read_csv)
+
+    vote_count = vote_count = 1
+
+    print(vote_count)
+
+
+
+
+
 
 
 
